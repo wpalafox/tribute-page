6dee59366e6e1797a6797b4872b95246b4d31c2c
wpalafox <wpalafox@gmail.com>
2020-03-01 17:17:13 -0600

    Tribute info added

---
+++ src/Profilepic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Profilepic() {
+    return (
+        <div className="img-div">
+            <img src="https://i.insider.com/5a58de42f421496e0f8b4848?width=1100" alt="kendrick lamar" id="image" class="img-thumbnail"></img>
+            <div id="img-caption">"We’re all human at the end of the day, making mistakes. But learn from them is the key."</div>
+        </div>
+    )
+}
+
+
---
+++ src/Tributeinfo.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Tributeinfo() {
+    return (
+        <div className="container">
+            <h1>Kendrick Lamar</h1>
+         <p>Kendrick Lamar Duckworth (born June 17, 1987) is an American rapper, 
+            songwriter, and record producer. He is regarded by many critics and contemporaries 
+            as one of the most important and influential rappers of his generation, and as one of the 
+            greatest rappers of all time. Raised in Compton, California, Lamar embarked on 
+            his musical career as a teenager under the stage name K-Dot, releasing a mixtape that garnered 
+            local attention and led to his signing with indie record label Top Dawg Entertainment (TDE). 
+            He began to gain recognition in 2010 after his first retail release, Overly Dedicated. 
+            The following year, he independently released his first studio album, Section.80, which 
+            included his debut single "HiiiPoWeR". By that time, he had amassed a large online following and 
+            collaborated with several prominent hip hop artists, including The Game, Busta Rhymes, and Snoop Dogg.
+            Lamar's major label debut album, Good Kid, M.A.A.D City, was released in 2012 by TDE, Aftermath, and 
+            Interscope Records to critical acclaim. It debuted at No. 2 on the U.S. Billboard 200 and was later 
+            certified platinum by the RIAA. The record contained the top 40 singles "Swimming Pools (Drank)", 
+            "Bitch, Don't Kill My Vibe", and "Poetic Justice". His critically acclaimed third album To Pimp a 
+            Butterfly (2015) incorporated elements of funk, soul, jazz, and spoken word. It debuted atop the 
+            charts in the U.S. and the UK, and won the Grammy Award for Best Rap Album at the 58th ceremony. 
+            In 2016, Lamar released Untitled Unmastered, a collection of unreleased demos that originated during 
+            the recording sessions for To Pimp a Butterfly. He released his fourth album, Damn, in 2017 to further 
+            acclaim; its lead single "Humble" topped the U.S. Billboard Hot 100 chart, while the album itself became 
+            the first non-classical and non-jazz album to be awarded the Pulitzer Prize for Music.
+            Aside from his solo career, Lamar is also known as a member of the hip hop supergroup Black Hippy, 
+            alongside his TDE label-mates Ab-Soul, Jay Rock, and Schoolboy Q. He has received many accolades over the 
+            course of his career, including 13 Grammy Awards and an Academy Award nomination. In 2012, MTV named him 
+            the "Hottest MC in the Game" on their annual list.Time named him one of the 100 most influential 
+            people in the world in 2016. </p>
+        </div>
+    )
+}
