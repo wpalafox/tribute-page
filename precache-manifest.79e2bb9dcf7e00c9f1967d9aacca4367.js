2287d18fc0ef43e743f14f37be57718b93c272e0
wpalafox <wpalafox@gmail.com>
2020-03-02 17:44:46 -0600

    Updates

---
+++ precache-manifest.79e2bb9dcf7e00c9f1967d9aacca4367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff3a567a98fc21aa0b6cbaf6405ac73",
+    "url": "/tribute-page/index.html"
+  },
+  {
+    "revision": "7f1c2648e19bc4796cfd",
+    "url": "/tribute-page/static/css/main.6458de76.chunk.css"
+  },
+  {
+    "revision": "1b57ee214300443b90de",
+    "url": "/tribute-page/static/js/2.f1c714e0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tribute-page/static/js/2.f1c714e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1c2648e19bc4796cfd",
+    "url": "/tribute-page/static/js/main.1f8c58c5.chunk.js"
+  },
+  {
+    "revision": "ec0cb0b85c30614b9e40",
+    "url": "/tribute-page/static/js/runtime-main.73c8e560.js"
+  }
+]);
